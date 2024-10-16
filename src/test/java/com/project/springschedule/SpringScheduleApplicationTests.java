fe33d47b3c338372af4d86f8e5872bf0a46d4a20
caose <rlagkrwls258@gmail.com>
2024-10-16 13:36:18 +0900

    feat : init commit

---
+++ src/test/java/com/project/springschedule/SpringScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
